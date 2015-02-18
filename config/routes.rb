996d23e64dea2c5d2824d1d3d6a4084ba6efdf44
Jmingus <jmingus@austin.rr.com>
2015-02-17 19:24:28 -0600

    set up figaro and paperclip, also set up scraps controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users do
     resources :scraps
   end
+  get 'scraps/new'
+
+  get 'scraps/edit'
+
+  get 'users/index'
+
+  get 'welcome/index'
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
