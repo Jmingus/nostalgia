feeec0dc9e9234844742a492ee48a05c26de49b5
Jmingus <jmingus@austin.rr.com>
2015-02-17 23:15:29 -0600

    Added style to views, also setup Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+2.times do
+  user = User.create!( first_name: Faker::Name.first_name,
+                       last_name: Faker::Name.last_name,
+                       user_name: Faker::Name.title,
+                       email: Faker::Internet.email,
+                       password: 'password',
+  )
+  6.times do
+    user.scraps.create( title: Faker::Lorem.words(4),
+                        scrap_picture: Faker::Avatar.image,
+                        caption: Faker::Lorem.words(10),
+
+    )
+  end
+end
